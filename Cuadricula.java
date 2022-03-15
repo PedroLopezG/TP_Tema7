fcb20a481132a73b3b0c0740e3017e93274fc2a3
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-03-15 13:21:02 +0100

    Ejercicio barcos empezado

---
+++ Cuadricula.java
@@ -0,0 +1,15 @@
+public class Cuadricula {
+    int tabla[][] = new int [10][10];
+
+    public void depositarBarco(){
+
+    }
+
+    public void disparar(){
+
+    }
+
+    public void visualizacion(){
+        
+    }
+}
